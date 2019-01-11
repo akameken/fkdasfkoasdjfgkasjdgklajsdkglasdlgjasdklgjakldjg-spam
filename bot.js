beba44c6807d81a4b37eaa2733b1364370e4c465
dldadnasjndfsjafajnsdfadnfadfkoafasdklk <43988221+dldadnasjndfsjafajnsdfadnfadfkoafasdklk@users.noreply.github.com>
2019-01-11 20:18:07 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,402 @@
+const Discord = require('discord.js');
+const prefix = "=";
+const server = "532736922397966336"; 
+const channel = "532807254035005441";
+const owns = "476503634411257858"
+const l1 = new Discord.Client()
+const l1token = process.env.BOT_TOKEN1
+const l2 = new Discord.Client()
+const l2token = process.env.BOT_TOKEN2
+const l3 = new Discord.Client()
+const l3token = process.env.BOT_TOKEN3
+const l4 = new Discord.Client()
+const l4token = process.env.BOT_TOKEN4
+const l5 = new Discord.Client()
+const l5token = process.env.BOT_TOKEN5
+const l6 = new Discord.Client()
+const l6token = process.env.BOT_TOKEN6
+const l7 = new Discord.Client()
+const l7token = process.env.BOT_TOKEN7
+const l8 = new Discord.Client()
+const l8token = process.env.BOT_TOKEN8
+const l9 = new Discord.Client()
+const l9token = process.env.BOT_TOKEN9
+const l10 = new Discord.Client()
+const l10token = process.env.BOT_TOKEN10
+const l11 = new Discord.Client()
+const l11token = process.env.BOT_TOKEN11
+const l12 = new Discord.Client()
+const l12token = process.env.BOT_TOKEN12
+const l13 = new Discord.Client()
+const l13token = process.env.BOT_TOKEN13
+const l14 = new Discord.Client()
+const l14token = process.env.BOT_TOKEN14
+const l15 = new Discord.Client()
+const l15token = process.env.BOT_TOKEN15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///----------------------------------------------------------------\\\
+l1.login(l1token)
+l1.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l1.on('ready', ready => {
+    console.log("OK BOT NUMBER 1 turned on " + `${l1.user.tag}`)
+    setInterval(()=>{
+    l1.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l1.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l2.login(l2token)
+l2.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l2.on('ready', ready => {
+    console.log("OK BOT NUMBER 2 turned on " + `${l2.user.tag}`)
+    setInterval(()=>{
+    l2.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l2.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l3.login(l3token)
+l3.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l3.on('ready', ready => {
+    console.log("OK BOT NUMBER 3 turned on " + `${l3.user.tag}`)
+    setInterval(()=>{
+    l3.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l3.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l4.login(l4token)
+l4.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l4.on('ready', ready => {
+    console.log("OK BOT NUMBER 4 turned on " + `${l4.user.tag}`)
+    setInterval(()=>{
+    l4.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l4.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l5.login(l5token)
+l5.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l5.on('ready', ready => {
+    console.log("OK BOT NUMBER 5 turned on " + `${l5.user.tag}`)
+    setInterval(()=>{
+    l5.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l5.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l6.login(l6token)
+l6.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l6.on('ready', ready => {
+    console.log("OK BOT NUMBER 6 turned on " + `${l6.user.tag}`)
+    setInterval(()=>{
+    l6.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l6.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l7.login(l7token)
+l7.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l7.on('ready', ready => {
+    console.log("OK BOT NUMBER 7 turned on " + `${l7.user.tag}`)
+    setInterval(()=>{
+    l7.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l7.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l8.login(l8token)
+l8.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l8.on('ready', ready => {
+    console.log("OK BOT NUMBER 8 turned on " + `${l8.user.tag}`)
+    setInterval(()=>{
+    l8.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l8.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l9.login(l9token)
+l9.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l9.on('ready', ready => {
+    console.log("OK BOT NUMBER 9 turned on " + `${l9.user.tag}`)
+    setInterval(()=>{
+    l9.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l9.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l10.login(l10token)
+l10.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l10.on('ready', ready => {
+    console.log("OK BOT NUMBER 10 turned on " + `${l10.user.tag}`)
+    setInterval(()=>{
+    l10.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l10.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l11.login(l11token)
+l11.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l11.on('ready', ready => {
+    console.log("OK BOT NUMBER 11 turned on " + `${l11.user.tag}`)
+    setInterval(()=>{
+    l11.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l11.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l12.login(l12token)
+l12.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l12.on('ready', ready => {
+    console.log("OK BOT NUMBER 12 turned on " + `${l12.user.tag}`)
+    setInterval(()=>{
+    l12.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l12.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l13.login(l13token)
+l13.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l13.on('ready', ready => {
+    console.log("OK BOT NUMBER 13 turned on " + `${l13.user.tag}`)
+    setInterval(()=>{
+    l13.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l13.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l14.login(l14token)
+l14.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l14.on('ready', ready => {
+    console.log("OK BOT NUMBER 14 turned on " + `${l14.user.tag}`)
+    setInterval(()=>{
+    l14.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l14.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+l15.login(l15token)
+l15.on('message', message => {
+    if(message.content.startsWith(prefix + "repeat")) {
+        var args = message.content.split(' ').slice(1).join(' ');
+        if(!args) return message.channel.send("** Type SomeThing , Sir !**")
+        if(!message.author.id === owns) return;
+        message.channel.send(args)
+        
+    }
+})
+l15.on('ready', ready => {
+    console.log("OK BOT NUMBER 15 turned on " + `${l15.user.tag}`)
+    setInterval(()=>{
+    l15.guilds.get(server).channels.get(channel).send(`BanderitaX LOL :D`)
+  },350);
+})
+l15.on('message', message => {
+    if(message.content.startsWith(prefix + "repme")) {
+        message.channel.send(`#rep <@${message.author.id}>`)
+        
+    }
+})
+
